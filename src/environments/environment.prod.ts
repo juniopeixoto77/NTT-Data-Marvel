d36b448d4c71a828f6bbf0780e0531d160d9ff7a
juniopeixoto77 <juniodiplomata@gmail.com>
2024-03-13 21:30:48 -0300

    Melhorias.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  publicKey: '396292dc2977f1e956a4cf99614e8d4f',
+  privateKey: '07b20a10cf4481a14dc1532d697df2ca572c3784'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  publicKey: '396292dc2977f1e956a4cf99614e8d4f',
+  privateKey: '07b20a10cf4481a14dc1532d697df2ca572c3784'
 };
 
 /*
